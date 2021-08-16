57896cf52041ebdb18cfff78cf2b6c044d901e45
Levi <muhammad.levi@sid-indonesia.org>
2021-08-16 13:53:48 +0700

    Affected Issue(s): #1577905028 #1577929004
    What this commit has achieved:
    1. Added expected-to-fail test

--- src/test/java/org/sidindonesia/bidanreport/controller/ExcelSheetControllerTests.java
+++ src/test/java/org/sidindonesia/bidanreport/controller/ExcelSheetControllerTests.java
@@ -1,11 +1,14 @@
 package org.sidindonesia.bidanreport.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.sidindonesia.bidanreport.IntegrationTest;
+import org.sidindonesia.bidanreport.exception.ExcelWriteException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -29,5 +32,21 @@ class ExcelSheetControllerTests {
 	void testDownloadAllTablesAsExcelSheets_withHappyFlow() throws Exception {
 		ResponseEntity<Resource> response = excelSheetController.downloadAllTablesAsExcelSheets();
 		assertThat(response.getBody()).isInstanceOf(InputStreamResource.class);
+	}
+}
+
+@IntegrationTest
+@Transactional
+@SpringBootTest(properties = "spring.jpa.properties.hibernate.hbm2ddl.auto=none")
+class ExcelSheetControllerFailingTests {
+
+	@Autowired
+	private ExcelSheetController excelSheetController;
+
+	@Test
+	void testDownloadAllTablesAsExcelSheets_withNoDatabaseTablesGenerated_thenExcelWriteExceptionWillBeThrown() {
+		assertThrows(ExcelWriteException.class, () -> {
+			excelSheetController.downloadAllTablesAsExcelSheets();
+		});
 	}
 }
