45d92e935d51975298bf0754d39787d9c80a7a30
Levi <muhammad.levi@sid-indonesia.org>
2021-08-18 13:04:11 +0700

    Affected Issue(s): #1577929004
    What this commit has achieved:
    1. Increase code coverage

--- src/test/java/org/sidindonesia/bidanreport/controller/ExcelSheetControllerTests.java
+++ src/test/java/org/sidindonesia/bidanreport/controller/ExcelSheetControllerTests.java
@@ -2,9 +2,12 @@ package org.sidindonesia.bidanreport.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.sidindonesia.bidanreport.IntegrationTest;
+import org.sidindonesia.bidanreport.domain.MotherIdentity;
+import org.sidindonesia.bidanreport.repository.MotherIdentityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -18,16 +21,37 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 class ExcelSheetControllerTests {
 
+	private static final Long DEFAULT_ID = 1L;
+
 	@Autowired
 	private ExcelSheetController excelSheetController;
 
+	@Autowired
+	private MotherIdentityRepository motherIdentityRepository;
+
+	private MotherIdentity motherIdentity;
+
 	@BeforeEach
 	public void setUp() {
+		createMotherIdentity();
+		motherIdentityRepository.save(motherIdentity);
+	}
+
+	@AfterEach
+	public void tearDown() {
+		motherIdentityRepository.delete(motherIdentity);
 	}
 
 	@Test
 	void testDownloadAllTablesAsExcelSheets_withHappyFlow() throws Exception {
 		ResponseEntity<Resource> response = excelSheetController.downloadAllTablesAsExcelSheets();
 		assertThat(response.getBody()).isInstanceOf(InputStreamResource.class);
+	}
+
+	private MotherIdentity createMotherIdentity() {
+		motherIdentity = new MotherIdentity();
+		motherIdentity.setEventId(DEFAULT_ID);
+		return motherIdentity;
+
 	}
 }
